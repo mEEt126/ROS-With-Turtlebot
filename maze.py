36b00c2ae0ab9010f229867b4878c18b7531f1c8
mEEtGandhi <37804920+mEEt126@users.noreply.github.com>
2020-05-02 01:28:26 +0530

    maze solving using laser data

---
+++ maze.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
+import time
+
+class maze_solving():
+	def __init__(self):
+		rospy.init_node('maze solving node using left hand algorithm')
+		self.vel_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size = 5 )
+		self.laser_subscriber = rospy.Subscriber('/kobuki/laser/scan', LaserScan, self.laser_callback)
+		self.cmd = Twist()
+        	self.laser_msg = LaserScan()
+		self.rate = rospy.Rate(10)
+		self.destination_flag = 0
+
+	def laser_callback(self, msg):
+        	self.laser_msg = msg
+
+	def get_laser(self, pos):
+        	time.sleep(1)
+       	 	return self.laser_msg.ranges[pos]
+
+	def stop_turtlebot(self):
+		self.cmd.linear.x = 0.0
+		self.cmd.angular.z = 0.0
+
+		self.vel_publisher.publish(self.cmd)
+		rospy.loginfo("Turtlebot Stopped.!")
+
+	def go_straight(self):
+		self.cmd.linear.x = 0.5
+		self.cmd.linear.y = 0
+		self.cmd.linear.z = 0
+		self.cmd.angular.x = 0
+		self.cmd.angular.y = 0
+		self.cmd.angular.z = 0
+
+		self.vel_publisher.publish(self.cmd)
+		rospy.loginfo("cmd Published")
+
+	def destination(self):
+		rospy.loginfo("Destination Reached.!")
+		self.destination_flag = 1
+
+
+	def node_detected(self):
+		self.stop_turtlebot()
+		left=self.get_laser(0)
+		straight=self.get_laser(360)
+		right=self.get_laser(719)
+		rospy.loginfo("left:" + str(left))
+		rospy.loginfo("straight:" + str(straight))
+		rospy.loginfo("right:" + str(right))
+		time.sleep(1)
+
+		
+
+	def main(self):
+		while(self.destination_flag != 1):
+			present_reading = self.get_laser(360)
+			rospy.loginfo(present_reading)
+			if (present_reading <= 1):
+				self.node_detected()
+			elif (present_reading == 'inf'):
+				self.destination()
+			else :
+				self.go_straight()
+		
+
+
+if __name__ == '__main__':
+    
+    Maze_solver_turtlebot = maze_solving()
+    try:
+        Maze_solver_turtlebot.main()
+
+    except rospy.ROSInterruptException:
+        pass
